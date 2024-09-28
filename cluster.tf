86c0067fc49964ac4745accf7538a11136b86187
Lucas <lucas.santiago@sgroup.com.br>
2024-09-28 15:08:11 -0300

    Teste criaao do cluster

--- cluster.tf
+++ cluster.tf
@@ -1,17 +1,21 @@
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
+
 locals {
   name   = "food-cluster"
 
-  vpc_cidr = "172.31.0.0/16"
+  vpc_cidr = "172.16.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
 
-  public_subnets  = ["172.31.64.0/20", "172.31.144.0/20"]
-  private_subnets = ["172.31.48.0/20", "172.31.0.0/20"]
-  intra_subnets   = ["172.31.80.0/20", "172.31.32.0/20"]
+  public_subnets  = ["172.16.64.0/20", "172.16.144.0/20"]
+  private_subnets = ["172.16.48.0/20", "172.16.0.0/20"]
+  intra_subnets   = ["172.16.80.0/20", "172.16.32.0/20"]
 
-  tags = {}
+  tags = {
+    Name        = local.name
+    Environment = "production"  # Defina o ambiente de forma adequada
+  }
 }
 
 data "aws_security_group" "existing_sg" {
@@ -41,49 +45,44 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
+
+  # Ative o mapeamento de IPs públicos nas sub-redes públicas
+  map_public_ip_on_launch = true
 }
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 9.15.1"  # Considere atualizar para uma versão mais recente
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+    coredns = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni = { most_recent = true }
   }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # Corrigido: Incluindo o atributo "type"
   cluster_security_group_additional_rules = {
     "allow_db_access" = {
-      type                            = "ingress"  # Define se a regra é de entrada (ingress) ou saída (egress)
+      type                            = "ingress"
       description                     = "Allow access to DB"
       from_port                        = 5432
       to_port                          = 5432
       protocol                         = "tcp"
-      cidr_blocks                      = ["0.0.0.0/0"]
+      cidr_blocks                      = ["172.16.0.0/16"]  # Restringir o acesso
       security_group_id                = [data.aws_security_group.existing_sg.id]
       source_cluster_security_group    = false
     }
   }
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["t2.micro"]
-
+    instance_types = ["t2.medium"]  # Ajuste o tipo de instância conforme necessidade
     attach_cluster_primary_security_group = true
   }
 
@@ -91,14 +90,21 @@ module "eks" {
     ascode-cluster-wg = {
       min_size     = 1
       max_size     = 9
-      desired_size = 1
-
-      instance_types = ["t2.micro"]
+      desired_size = 3  # Ajuste conforme necessário
+      instance_types = ["t2.medium"]  # Instâncias ajustadas para carga de trabalho
       capacity_type  = "ON_DEMAND"
-
-      tags = {}
+      tags = local.tags
     }
   }
 
   tags = local.tags
+}
+
+# Saídas para informações úteis
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_id
 }
