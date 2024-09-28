886a09b6ebdc9bebc2e76a82562c8f8d766f660c
Lucas <lucas.santiago@sgroup.com.br>
2024-09-27 22:03:18 -0300

    Primeiro commit EKS - Kubernetes

--- cluster.tf
+++ cluster.tf
@@ -6,12 +6,12 @@ locals {
   name   = "food-cluster"
   region = "us-east-1"
 
-  vpc_cidr = "10.123.0.0/16"
+  vpc_cidr = "172.31.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
 
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  public_subnets  = ["subnet-056aa0bda01905c8f", "subnet-073805c092643f2f3"]
+  private_subnets = ["subnet-056f9754ec5f40f32", "subnet-0a8a347d1a8d57ff9"]
+  intra_subnets   = ["subnet-05f43d62fa98a1a89", "subnet-041f0830fe1737b2e"]
 
   tags = {
 
@@ -63,6 +63,18 @@ module "eks" {
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
+
+
+ cluster_security_group_additional_rules = [{
+    description                     = "Allow access to DB"
+    from_port                        = 5432
+    to_port                          = 5432
+    protocol                         = "tcp"
+    cidr_blocks                      = ["0.0.0.0/0"]
+    security_group_id                = aws_security_group.aurora_sg.id
+    source_cluster_security_group    = false
+  }]
+
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
